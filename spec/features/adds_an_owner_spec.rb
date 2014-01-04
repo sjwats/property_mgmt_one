5897aaca4e864b9e76ad100b7edfb0712e0981f3
John <s.johnwatson@gmail.com>
2014-01-03 22:53:51 -0500

    adds an owner feature now working and tests passing for valid and invalid input

---
+++ spec/features/adds_an_owner_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+feature 'record an owner' do
+  scenario 'with valid attribute input' do
+    visit '/'
+    click_on 'View Owners'
+    click_link 'Add New Owner'
+    fill_in 'First name', with: 'Michael'
+    fill_in 'Last name', with: 'Bluth'
+    fill_in 'Email', with: 'michael@bluth.com'
+    fill_in 'Company name', with: 'Bluth Company'
+    click_button 'Add Owner'
+    expect(page).to have_content('Owner Added!')
+
+  end
+
+  scenario 'with invalid input' do
+    visit '/'
+    click_on 'View Owners'
+    click_link 'Add New Owner'
+    click_button 'Add Owner'
+
+    expect(page).to_not have_content('Owner Added!')
+
+    within ".input.owner_first_name" do
+      expect(page).to have_content "can't be blank"
+    end
+
+    within ".input.owner_last_name" do
+      expect(page).to have_content "can't be blank"
+    end
+
+    within ".input.owner_email" do
+      expect(page).to have_content "can't be blank"
+    end
+  end
+
+end
